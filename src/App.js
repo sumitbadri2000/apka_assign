6f332e5b4f1c621d93c3642e5b287af01fc434a7
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-12-08 16:48:54 +0530

    apkapay_assign

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from "react";
 import SearchBus from "./Components/SearchBus";
 import Navbar from "./Components/Navbar";
 import { Box } from "@chakra-ui/react";
-import SeatAvailabilityComponent from "./Components/Layout";
+import SeatBookingSystem from "./Components/Layout";
 // import ListBus from "./components/ListBus";
 // import SeatLayoutSelection from "./components/SeatLayoutSelection";
 
 const App = () => {
   return (
     <Box bg="rgb(196,196,196)" padding={6}>
-      {/* <Navbar />
-      <SearchBus /> */}
-      <SeatAvailabilityComponent />
+      <Navbar />
+      <SearchBus />
+      {/* <SeatBookingSystem /> */}
     </Box>
   );
 };
